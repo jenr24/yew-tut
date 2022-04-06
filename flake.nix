d79bbb297217dc6e4d9d9d464bf1dd63ed102d27
Jen Reiss <jenc.reiss24@gmail.com>
2022-04-06 13:52:58 -0500

    added toolchain

--- flake.nix
+++ flake.nix
@@ -1,22 +1,55 @@
 {
-    description = "Rust Yew learning project"
+    description = "Rust Yew learning project";
     
     inputs = {
         flake-utils.url = "github:numtide/flake-utils";
-        nixpkgs.url = "nixpkgs/nixpkgs-21.11";
+        nixpkgs.url = "github:NixOS/nixpkgs/release-21.11";
+        rust-overlay = {
+            url = "github:oxalica/rust-overlay";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
 
-    outputs = { self, nixpkgs, flake-utils }:
+    outputs = { self, nixpkgs, flake-utils, rust-overlay }:
         flake-utils.lib.eachDefaultSystem (system:
-            let pkgs = import nixpkgs { inherit system; }; in {
+            let 
+                overlays = [ rust-overlay.overlay ];
+                pkgs = import nixpkgs { inherit overlays system; }; 
+                rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+                inputs = [ rust pkgs.wasm-bindgen-cli ];
+            in {
                 defaultPackage = pkgs.rustPlatform.buildRustPackage {
                     pname = "rust-yew-tut";
                     version = "0.0.1";
+
                     src = ./.;
 
                     cargoLock = {
                         lockFile = ./Cargo.lock;
                     };
+
+                    nativeBuildInputs = inputs;
+
+                    buildPhase = ''
+                        cargo build --release --target=wasm32-unknown-unknown
+                        echo 'Creating out dir...'
+                        mkdir -p $out/src;
+                        # Optional, of course
+                        # echo 'Copying package.json...'
+                        # cp ./package.json $out/;
+                        echo 'Generating node module...'
+                        wasm-bindgen \
+                        --target nodejs \
+                        --out-dir $out/src \
+                        target/wasm32-unknown-unknown/release/gcd.wasm;
+                    '';
+
+                    installPhase = "echo 'Skipping installPhase'";
+                };
+
+                devShell = with pkgs; mkShell {
+                    buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy trunk wasm-bindgen-cli ];
+                    RUST_SRC_PATH = rustPlatform.rustLibSrc;
                 };
             }
         );
